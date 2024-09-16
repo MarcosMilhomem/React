d7d9b92deb46a2b90fb97c20e6e76c1ddcda1554
Marcos Apolo <marcosapolo511@gmail.com>
2024-09-16 01:44:33 -0300

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
   const name = 'Mateus'
@@ -23,6 +24,7 @@ function App() {
       <HelloWorld/>
       <SayMyName nome="Apolo"/>
       <SayMyName nome={nome}/>
+      <Pessoa nome="Rodrigo" idade="23" profissao="Programador" foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
