9da6c76358e4bfedc74c2454cea691eae6135bb2
Marcos Apolo <marcosapolo511@gmail.com>
2024-10-11 01:55:15 -0300

    reacr

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,4 +1,16 @@
-function Home () {
-    return (<h1>Home</h1>)
+import style from './Home.modules.css'
+import savings from '../../../src/img/savings.svg'
+
+function Home() {
+    return (
+        <section>
+            <h1>
+                Bem-vindo ao <span>Costs</span>
+            </h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo</p>
+            <a href="/">Criar Projeto</a>
+            <img src={savings} alt ="Cos    ts" />
+        </section>
+    )
 }
 export default Home
