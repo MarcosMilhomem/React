d15d5590b392569d43682eba30cfde3f117c2cfa
Marcos Apolo <marcosapolo511@gmail.com>
2024-10-11 19:21:27 -0300

    react

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,15 +1,17 @@
-import style from './Home.modules.css'
+import style from './Home.module.css'
+import LinkButton from '../LinkButton'
 import savings from '../../../src/img/savings.svg'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <section>
+        <section className={style.home_container}>
             <h1>
                 Bem-vindo ao <span>Costs</span>
             </h1>
             <p>Comece a gerenciar os seus projetos agora mesmo</p>
-            <a href="/">Criar Projeto</a>
-            <img src={savings} alt ="Cos    ts" />
+            <LinkButton to="/newproject" text="Criar Projeto"/>
+            <img src={savings} alt ="Costs" />
         </section>
     )
 }
