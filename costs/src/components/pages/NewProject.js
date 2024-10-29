c351839ae9bcd0bff828edafbb0e2c92c4ba0b7d
Marcos Apolo <marcosapolo511@gmail.com>
2024-10-29 01:39:42 -0300

    react

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -5,7 +5,7 @@ function NewProject () {
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para deppois adicionar os serviços</p>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
             <ProjectForm btnText="Criar Projeto"/>
         </div>
     )
