d58a990f135efabbb03b25037327c283816539b2
Marcos Apolo <marcosapolo511@gmail.com>
2024-11-07 15:45:33 -0300

    react

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,27 @@
+import { useState } from "react"
+
 import style from "./ProjectForm.module.css"
 import Input from "../forms/Input"
 import Select from "../forms/Select"
 import SubmitButton from "../forms/SubmitButton"
+import { useEffect } from "react"
 
 function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch ( "http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+        
+        }).then((res)=>res.json())
+        .then((data)=>{setCategories(data)})
+    })
+    
+
     return (
         <form className={style.form}>
             <Input
@@ -19,7 +37,7 @@ function ProjectForm({ btnText }) {
                 placeholder="Insira o orçamento total" />
 
             <Select name="category_id" text="Selecione a categoria" />
-
+            {JSON.stringify(categories)}
             <SubmitButton text={btnText} />
         </form>
     )
